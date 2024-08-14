d122b7d5970a0ff15c127c551a6dcba9b9eb4ec5
Brett Chalupa <brettchalupa@gmail.com>
2024-08-14 13:48:21 -0400

    remove .DS_Store files from assets in wasm release script

--- release_wasm.sh
+++ release_wasm.sh
@@ -11,6 +11,7 @@ cp web/*js web-release
 cp web/index.html web-release
 mkdir -p web-release/assets
 cp -r assets/* web-release/assets
+rm -rf web-release/**/.DS_Store
 zip -r web-release.zip web-release
 echo "web release zipped into web-release.zip"
 butler push web-release.zip brettchalupa/sokoworld:html
