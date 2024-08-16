bb65a18aca7f31ff94b420ea3224c6af4c6d082c
Brett Chalupa <brettchalupa@gmail.com>
2024-08-16 15:32:23 -0400

    persist & load settings from disk
    this is pretty rudimentary, but it works for WASM & desktop which is
    good enough for now!
    
    quad-storage works out of the box, but it has some limitations like the
    location is always set to `local.data` on desktop.
    
    in a more ideal world, I think the data structure would be verified and
    backed by a bit more

--- src/settings.rs
+++ src/settings.rs
@@ -2,17 +2,33 @@ use macroquad::window::set_fullscreen;
 
 /// user-set options to customize the experience to their liking
 pub struct Settings {
+    /// whether or not audio should play
     mute: bool,
+    /// whether or not the window should take up the entire screen
     fullscreen: bool,
 }
 
+const FULLSCREEN: &str = "fullscreen";
+const MUTE: &str = "mute";
+
 impl Settings {
     pub fn load() -> Self {
-        // TODO: load these from somewhere
-        Self {
-            mute: false,
-            fullscreen: false,
+        let mut fullscreen = false;
+        let mut mute = false;
+
+        let storage = &mut quad_storage::STORAGE.lock().unwrap();
+        if let Some(storage_fullscreen) = storage.get(FULLSCREEN) {
+            fullscreen = storage_fullscreen == "true";
         }
+        if let Some(storage_mute) = storage.get(MUTE) {
+            mute = storage_mute == "true";
+        }
+
+        if fullscreen {
+            set_fullscreen(fullscreen);
+        }
+
+        Self { mute, fullscreen }
     }
 
     pub fn is_fullscreen(&self) -> bool {
@@ -25,12 +41,16 @@ impl Settings {
 
     pub fn toggle_mute(&mut self) -> bool {
         self.mute = !self.mute;
+        let storage = &mut quad_storage::STORAGE.lock().unwrap();
+        storage.set(MUTE, self.mute.to_string().as_str());
         self.mute
     }
 
     pub fn toggle_fullscreen(&mut self) -> bool {
         self.fullscreen = !self.fullscreen;
         set_fullscreen(self.fullscreen);
+        let storage = &mut quad_storage::STORAGE.lock().unwrap();
+        storage.set(FULLSCREEN, self.mute.to_string().as_str());
         self.fullscreen
     }
 }
