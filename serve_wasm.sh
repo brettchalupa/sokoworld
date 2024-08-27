ac8328577ca8affe383cb7e346d4ad2d811b0046
Brett Chalupa <brettchalupa@gmail.com>
2024-08-26 21:39:02 -0400

    swap local wasm web server to use a rust crate
    so someone doesn't have to have Ruby installed

--- serve_wasm.sh
+++ serve_wasm.sh
@@ -1,8 +1,5 @@
 set -e
 
-# requires Ruby to be installed
-# TODO: use a simple Rust web server
-
 ./build_wasm.sh
 cd web
-PORT=5577 ruby -run -e httpd . -p ${PORT:=5577}
+simple-http-server --index -p ${PORT:=5577} .
